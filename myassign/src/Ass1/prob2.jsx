10dbf8fb5ab2650b92c66d481edbd1c91861da73
Preeti Sharma <preetbhardwaj517@gmail.com>
2022-04-09 16:01:13 +0530

    firstassignment

--- myassign/src/Ass1/prob2.jsx
+++ myassign/src/Ass1/prob2.jsx
@@ -5,20 +5,20 @@
          return(
              <>
              <div className="btn">
-                 <button>JOIN US</button>
-                 <button>SETTINGS</button>
+                 <button className="eachbtn" style={{backgroundColor: "lightblue"}}>JOIN US</button>
+                 <button className="eachbtn" style={{backgroundColor:"#3f4e5f"}}>SETTINGS</button>
              </div>
              <div className="btn">
-             <button>LOGIN</button>
-                 <button>CONTACT US</button>
+             <button className="eachbtn" style={{backgroundColor:"#dcae0c"}}>LOGIN</button>
+                 <button className="eachbtn" style={{backgroundColor:"#870515"}}>CONTACT US</button>
              </div>
              <div className="btn">
-             <button>SEARCH</button>
-                 <button>HELP</button>
+             <button className="eachbtn" style={{backgroundColor:"#06730c"}}>SEARCH</button>
+                 <button className="eachbtn" style={{backgroundColor:"#420673"}}>HELP</button>
              </div>
              <div className="btn">
-             <button>HOME</button>
-                 <button>DOWNLOAD</button>
+             <button className="eachbtn" style={{backgroundColor:"#d30ec8"}}>HOME</button>
+                 <button className="eachbtn" style={{backgroundColor:"#d37b0e"}}>DOWNLOAD</button>
              </div>
              </>
          )
